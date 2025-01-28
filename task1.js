6b7503c5d9a110d417338b59f6038e6492e4971b
Shifna21 <shifnamc556@gmail.com>
2025-01-28 16:19:27 +0530

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,32 @@
+// "  hello world"
+
+// Write a function
+// 1.convert a given string to uppercase.
+// 2.Find the Length of a String
+// 3.check if the word "hello" exists in a given string.
+// 4.Remove Extra Spaces
+// 5.replace the word "world" with "World"
+// 6.Get the First Character of string
+// 7.Convert first letter to upper case
+// 8.append "!!!" to the end of a given string.
+
+
+
+var Name="hello world"
+console.log(Name);
+console.log(Name.toUpperCase());
+console.log(Name.length);
+console.log(Name.includes("hello"));
+console.log(Name.trim());
+console.log(Name.replace("world","World"));
+console.log(Name.slice(0,1));
+console.log(Name.replace("hello","Hello"));
+console.log(Name.concat(""," !!!"));
+
+
+
+
+
+
+
+
