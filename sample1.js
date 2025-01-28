21b4bf6bace166b74f0b68e6c5e1deecee38cc2e
Shifna21 <shifnamc556@gmail.com>
2025-01-28 16:20:17 +0530

    js

---
+++ sample1.js
@@ -0,0 +1,52 @@
+// function add(val1,val2){
+//     console.log(val1+val2);
+    
+// }
+// add("2",3)
+// add(5,2)
+// add("hello",2)
+// add("")
+
+// var name="aji"
+
+// console.log(typeof name);
+
+// var user={
+//     name: "joy",
+//     age:34
+// }
+
+// console.log(typeof user);
+
+// var numbers=[1,2,4,6]
+
+// console.log(numbers[2]);
+
+// function sayName(name,age){
+//     console.log('my name :' + name + '  age :' + age);
+    
+// }
+// sayName('arun',44)
+
+// var user={
+//     name: "joyi",
+//     age:20
+// }
+
+// const arr=[22,33]
+
+// function nameA(obj,arr) {
+//     console.log('name :', obj.name);
+//     console.log('array va :', arr[0]);
+    
+// }
+// nameA(user,arr)
+
+
+var name='   my World  '
+console.log(name.trim());
+console.log(name.indexOf('W'));
+console.log(name.includes('n'));
+
+
+
